facae8808ef8bafd502728ffd4520de774d629b4
MahdiBahsoun <mahdi.bahsoun@gmail.com>
2018-12-09 08:42:14 +0200

    initial release

---
+++ WordToText/WordToText.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WordToText
+{
+    partial class WordToText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectWordFiles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectWordFiles
+            // 
+            this.btnSelectWordFiles.Location = new System.Drawing.Point(165, 59);
+            this.btnSelectWordFiles.Name = "btnSelectWordFiles";
+            this.btnSelectWordFiles.Size = new System.Drawing.Size(82, 34);
+            this.btnSelectWordFiles.TabIndex = 7;
+            this.btnSelectWordFiles.Text = "Browse";
+            this.btnSelectWordFiles.UseVisualStyleBackColor = true;
+            this.btnSelectWordFiles.Click += new System.EventHandler(this.btnSelectWordFiles_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select Word Files:";
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(115, 157);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(82, 45);
+            this.btnExtract.TabIndex = 5;
+            this.btnExtract.Text = "Extract Text";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // WordToText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSelectWordFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExtract);
+            this.Name = "WordToText";
+            this.Text = "WordToText";
+            this.Load += new System.EventHandler(this.WordToText_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectWordFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExtract;
+    }
+}
+
---
+++ WordToText/WordToText.cs
@@ -0,0 +1,149 @@
+﻿//Added references:
+//DocumentFormat.OpenXml.dll for "WordprocessingDocument"
+//WindowsBase.dll for "using"
+
+using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WordToText
+{
+    public partial class WordToText : Form
+    {
+        public WordToText()
+        {
+            InitializeComponent();
+        }
+
+        List<String> WordFilesPathes = new List<String>();
+        private void btnSelectWordFiles_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var dlg = new OpenFileDialog();
+                dlg.Filter = "Word Documents|*.doc;*.docx";
+                dlg.Multiselect = true;
+
+                if (dlg.ShowDialog() != DialogResult.OK)
+                    return;
+
+                WordFilesPathes = dlg.FileNames.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured: " + ex.Message);
+            }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (WordFilesPathes.Count <= 0)
+                {
+                    MessageBox.Show("No files are selected!");
+                    return;
+                }
+
+                string selectedDestination;
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    fbd.Description = "Save Extracted Text File At:";
+
+                    DialogResult result = fbd.ShowDialog();
+                    if (result != DialogResult.OK || string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        return;
+                    }
+                    selectedDestination = fbd.SelectedPath;
+                }
+
+                List<FileInfo> files = new List<FileInfo>();
+                //add files into list
+                foreach (var path in WordFilesPathes)
+                {
+                    FileInfo file = new FileInfo(path);
+
+                    string fileName = file.Name; //file name with extension
+                    string targetFileName = Path.GetFileNameWithoutExtension(fileName) + ".txt"; //target file name with new extension
+
+                    string txt = getTextFromWord(path);//extract text from Word File
+                    if (String.IsNullOrEmpty(txt))
+                    {
+                        //MessageBox.Show("Empty File!");
+                        continue;
+                    }
+
+                    //save extracted text into .txt file
+                    File.WriteAllText(selectedDestination + "\\" + targetFileName, txt);
+                }
+
+                WordFilesPathes.Clear();
+                MessageBox.Show("Done!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured: " + ex.Message);
+            }
+        }
+
+        public string getTextFromWord(string filePath)
+        {
+            try
+            {
+                //shout out to KyleM - Stack Overflow
+                const string wordmlNamespace = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+
+                StringBuilder textBuilder = new StringBuilder();
+                using (WordprocessingDocument wdDoc = WordprocessingDocument.Open(filePath, false))
+                {
+                    // Manage namespaces to perform XPath queries.  
+                    NameTable nt = new NameTable();
+                    XmlNamespaceManager nsManager = new XmlNamespaceManager(nt);
+                    nsManager.AddNamespace("w", wordmlNamespace);
+
+                    // Get the document part from the package.  
+                    // Load the XML in the document part into an XmlDocument instance.  
+                    XmlDocument xdoc = new XmlDocument(nt);
+                    xdoc.Load(wdDoc.MainDocumentPart.GetStream());
+
+                    XmlNodeList paragraphNodes = xdoc.SelectNodes("//w:p", nsManager);
+                    foreach (XmlNode paragraphNode in paragraphNodes)
+                    {
+                        XmlNodeList textNodes = paragraphNode.SelectNodes(".//w:t", nsManager);
+                        foreach (System.Xml.XmlNode textNode in textNodes)
+                        {
+                            textBuilder.Append(textNode.InnerText);
+                        }
+                        textBuilder.Append(Environment.NewLine);
+                    }
+
+                    wdDoc.MainDocumentPart.GetStream().Flush();
+                    wdDoc.MainDocumentPart.GetStream().Close();
+                }
+
+                return textBuilder.ToString();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error Occured on file: " + filePath + ". " + e.Message);
+                return "";
+            }
+        }
+
+        private void WordToText_Load(object sender, EventArgs e)
+        {
+
+        }
+ 
+    }
+}
